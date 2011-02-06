3d24c267e244a88ef8211aa3a4840dc09bca23c8
Your Name <frzdyb@gmail.com>
2011-02-06 16:56:13 +0100

    Changes made by sehansen, results in a solution prefered by the men

--- GS.py
+++ GS.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 men = {'Thor': [0,5], 'Loke': [0,5], 'Frej':[0,5],'Odin':[0,5],'Balder':[0,5]}
 women = {'Freja':0,'Sif':0,'Idun':0,'Frigg':0,'Gerd':0}
 Thor = ['Sif','Freja','Frigg','Gerd','Idun']
@@ -15,7 +16,7 @@ mnames = {'Thor':Thor,'Loke':Loke,'Frej':Frej,'Odin':Odin,'Balder':Balder}
 while 0 in [i[0] for i in men.values() if i[1] > 0]: #As long as there exists a single man who still has someone left to propose to
     for m in men.keys():
         if men[m][1]>0 and men[m][0] == 0: # We choose such a man (who is single, and still has options)
-            k = mnames[m][men[m][1]-1] # We choose his most prefered woman)
+            k = mnames[m][0] # We choose his most prefered woman)
         else:
             continue
         if women[k] == 0: # If the woman is single
